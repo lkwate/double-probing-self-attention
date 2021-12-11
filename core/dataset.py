dca6de6bd2262d9ec636a1ec38a3d4fee2196620
lkwate <kwatedassiloic@gmail.com>
2021-12-12 00:04:03 +0100

    prepare dataset

--- core/dataset.py
+++ core/dataset.py
@@ -63,6 +63,7 @@ class DataCollator:
 
 class MNLILightningDataModule(pl.LightningDataModule):
     def __init__(self, model_name, batch_size, num_workers):
+        super().__init__()
         self.model_name = model_name
         self.batch_size = batch_size
         self.num_workers = num_workers
